8973ae5be94cb2801f4531f113ff6fa31b4b3814
gvysotin <spideful@yandex.ru>
2024-06-28 21:08:40 +0800

    ssh1111112

--- app/Http/Controllers/SSHController.php
+++ app/Http/Controllers/SSHController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class SSHController extends Controller
 {
-    public function sshGitFromLaravel111111()
+    public function sshGitFromLaravel11111()
     {
         return '11111';
     }
